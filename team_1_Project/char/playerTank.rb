986d4b96f19f8635f1752fd117aff602d78265a2
Ashwin <ashwinrockon123@gmail.com>
2022-08-30 13:26:47 +0530

    Added instance methods to playerTank class

--- team_1_Project/char/playerTank.rb
+++ team_1_Project/char/playerTank.rb
@@ -1,19 +1,41 @@
 class PlayerTank < Sprite
+    @movingStatus
+    @currentPosX
+    @currentPosY
+    @img_width
+    @img_height
+    @isPotrait
+
     def initialize
         image = Image.load("image/player_tank.png")
-        x = 0
-        y = 230
+        @img_width = image.width
+        @img_height = image.height
+        @movingStatus = true
+        @currentPosX = 0
+        @currentPosY = 230
+        @isPotrait = false
 
-        super(x,y,image)
+        super(@currentPosX,@currentPosY,image)
     end
 
+    def setMovingStatus(status)
+        @movingStatus = status
+
+        if(@movingStatus == false)
+            puts "SET TO FALSE ==================="
+        end
+    end
+
+    def getMovingStatus
+        return @movingStatus
+    end
     def move
         i_x = Input.x
         i_y = Input.y 
         rotAngle = self.angle 
 
         # If the user presses both the movement keys a time, it should not move
-        if(i_x != 0 && i_y != 0)
+        if(!@movingStatus || i_x != 0 && i_y != 0)
             return
         end
 
@@ -21,23 +43,36 @@ class PlayerTank < Sprite
         case(i_y)
         when -1
             self.angle=(-90)
+            @isPotrait = true
         when 1
             self.angle=(90)
+            @isPotrait = true
         end    
 
         # Horizontal Direction Change
         case(i_x)
         when -1
             self.angle=(180)
+            @isPotrait = false
         when 1
             self.angle=(0)
+            @isPotrait = false
         end
 
         self.x += i_x
         self.y += i_y
 
 
-        puts "Angle of Rotation : #{self.angle}, x_input : #{Input.x}, y_input : #{Input.y}"
+        puts "Angle of Rotation : #{self.angle}, x : #{self.x}, y : #{self.y}"
+    end
+
+    def getImageSize
+        return @isPotrait, @img_width, @img_height
+    end
+
+    def setImageSize(width,height)
+        @img_width = width
+        @img_height = height
     end
 
 end
