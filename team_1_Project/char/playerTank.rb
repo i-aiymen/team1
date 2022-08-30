f04fcb1401385224a8648103b42679e591b7cf60
Ashwin <ashwinrockon123@gmail.com>
2022-08-30 07:49:09 +0530

    Added Rotation feature to the playerTank

--- team_1_Project/char/playerTank.rb
+++ team_1_Project/char/playerTank.rb
@@ -7,4 +7,37 @@ class PlayerTank < Sprite
         super(x,y,image)
     end
 
+    def move
+        i_x = Input.x
+        i_y = Input.y 
+        rotAngle = self.angle 
+
+        # If the user presses both the movement keys a time, it should not move
+        if(i_x != 0 && i_y != 0)
+            return
+        end
+
+        # Vertical Direction Change
+        case(i_y)
+        when -1
+            self.angle=(-90)
+        when 1
+            self.angle=(90)
+        end    
+
+        # Horizontal Direction Change
+        case(i_x)
+        when -1
+            self.angle=(180)
+        when 1
+            self.angle=(0)
+        end
+
+        self.x += i_x
+        self.y += i_y
+
+
+        puts "Angle of Rotation : #{self.angle}, x_input : #{Input.x}, y_input : #{Input.y}"
+    end
+
 end
