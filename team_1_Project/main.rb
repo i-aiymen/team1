2c0ebed7b00af33fbf6bfa2e3a6e4496547f0ed8
Ashwin <ashwinrockon123@gmail.com>
2022-08-30 06:56:02 +0530

    Added the playerTank file into main

--- team_1_Project/main.rb
+++ team_1_Project/main.rb
@@ -1,4 +1,5 @@
 require 'dxruby'
+require_relative './char/playerTank.rb'
 require_relative './level/level1'
 
 #window settings
@@ -7,6 +8,9 @@ Window.height = 676
 Window.caption = "Shooting Game"
 Window.bgcolor = C_WHITE
 
+playerTank = PlayerTank.new
+
 Window.loop do
     Level1.draw
+    playerTank.draw
 end
