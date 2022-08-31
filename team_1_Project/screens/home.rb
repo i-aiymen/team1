b87c4cce98974a90278054cd7afaf8a76c0b0fc9
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-08-31 22:21:41 +0530

    Added: Hover eefect to play button in homeScreen

--- team_1_Project/screens/home.rb
+++ team_1_Project/screens/home.rb
@@ -13,21 +13,21 @@ class Home
 
     
 
-    # def update(x, y)
-    #     m = Sprite.new(x, y, Image.new(1, 1, C_RED))
-    #     if m === @@playButton
-    #         @@playButton = Sprite.new(290,372, Image.load('image/playHover.png'))
-    #     end
-    #     # if m === @@replayButton
-    #     #     @@replayButton = Sprite.new(370,380, Image.load('image/replayHover.png'))
-    #     # end
-    # end
+    def update(x, y)
+        m = Sprite.new(x, y, Image.new(1, 1, C_RED))
+        if m === @@playButton
+            @@playButton = Sprite.new(290,372, Image.load('image/playHover.png'))
+        end
+        # if m === @@replayButton
+        #     @@replayButton = Sprite.new(370,380, Image.load('image/replayHover.png'))
+        # end
+    end
 
     def draw
-        # x = Input.mouse_pos_x
-        # y = Input.mouse_pos_y
+        x = Input.mouse_pos_x
+        y = Input.mouse_pos_y
 
-        # update(x, y)
+        update(x, y)
         Window.draw(0, 0, @@backgroundImage)
         Window.draw(212,241,@@gameNameText)
         Sprite.draw([@@playButton,@@settingsButton,@@rulesButton,@@musicButton,@@exitButton])
