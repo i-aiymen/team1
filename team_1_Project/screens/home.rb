2d2c71d7c4c6fd4f17330043f3287555b0e7e785
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-08-31 22:20:41 +0530

    Added: settings and rules button homeScreen

--- team_1_Project/screens/home.rb
+++ team_1_Project/screens/home.rb
@@ -4,20 +4,21 @@ class Home
     @@gameNameText = Image.load('image/gameName_text.png')
 
     def initialize
-        # @@homeButton = Sprite.new(290,380, Image.load('image/home.png'))
-        # @@replayButton = Sprite.new(370,380, Image.load('image/replay.png'))
+        @@playButton = Sprite.new(290,372, Image.load('image/play.png'))
+        @@settingsButton = Sprite.new(13,17, Image.load('image/settings.png'))
+        @@rulesButton = Sprite.new(13,80, Image.load('image/rules.png'))
     end
 
     
 
     # def update(x, y)
     #     m = Sprite.new(x, y, Image.new(1, 1, C_RED))
-    #     if m === @@homeButton
-    #         @@homeButton = Sprite.new(290,380, Image.load('image/homeHover.png'))
+    #     if m === @@playButton
+    #         @@playButton = Sprite.new(290,372, Image.load('image/playHover.png'))
     #     end
-    #     if m === @@replayButton
-    #         @@replayButton = Sprite.new(370,380, Image.load('image/replayHover.png'))
-    #     end
+    #     # if m === @@replayButton
+    #     #     @@replayButton = Sprite.new(370,380, Image.load('image/replayHover.png'))
+    #     # end
     # end
 
     def draw
@@ -27,6 +28,6 @@ class Home
         # update(x, y)
         Window.draw(0, 0, @@backgroundImage)
         Window.draw(212,241,@@gameNameText)
-        #Sprite.draw([@@homeButton,@@replayButton])
+        Sprite.draw([@@playButton,@@settingsButton,@@rulesButton])
     end
 end
