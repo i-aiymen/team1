92f956649452dc1650cd47de5e1c562667433e62
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-08-31 07:20:15 +0530

    Added: gameover text

--- team_1_Project/screens/gameOver.rb
+++ team_1_Project/screens/gameOver.rb
@@ -2,9 +2,11 @@ class GameOver
     @@backgroundImage = Image.load('image/background.png')
     @@homeButton = Image.load('image/home.png')
     @@replayButton = Image.load('image/replay.png')
+    @@gameoverText = Image.load('image/gameover_text.png')
 
     def self.draw
         Window.draw(0, 0, @@backgroundImage)
+        Window.draw(162,227,@@gameoverText)
         Window.draw(290,380,@@homeButton)
         Window.draw(370,380,@@replayButton)
     end
