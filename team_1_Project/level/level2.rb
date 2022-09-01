04bc82035703489453e042ba6786e075472ac4bc
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-02 03:17:18 +0530

    Updated: Player shooting bullets in all 4 directions

--- team_1_Project/level/level2.rb
+++ team_1_Project/level/level2.rb
@@ -5,11 +5,13 @@ class Level2
     @@enemyX = 590
     @@enemyY = 51
     @@playerDirection = 0
+    @@playerBullets = []
     @@enemyData = Enemy.new(30,10)
 
     @@backgroundImageLvl2 = Image.load('image/backgroundLvl2.png')
-    @@playerImage = Image.load('image/playerYoru.png')
     @@enemyImage = Image.load('image/enemyRobot.png')
+
+    
 
     def self.playerMovement
         if Input.key_push?(K_W) && @@playerY >= 10
@@ -29,7 +31,7 @@ class Level2
             @@playerDirection = 1
         end
         if Input.key_push?(K_J) && $player.mp >= 5
-            @@playerBullets.push([@@playerX, @@playerY, @@playerDirection])
+            @@playerBullets.push([@@playerX + 50, @@playerY + 50, @@playerDirection])
             $player.mp = $player.mp - 5
         end
         if Input.key_push?(K_K) && $player.mp >= 7
@@ -38,8 +40,31 @@ class Level2
         end
     end
 
+    def self.bulletMovements
+        num = 0
+        @@playerBullets.size.times do |i|
+            ch = i - num
+            case(@@playerBullets[ch][2])
+                when 0
+                    @@playerBullets[ch][1] -= 1
+                when 1
+                    @@playerBullets[ch][0] += 1
+                when 2
+                    @@playerBullets[ch][1] += 1
+                when 3
+                    @@playerBullets[ch][0] -= 1
+            end
+
+            if @@playerBullets[ch][0] < -75 || @@playerBullets[ch][0] > 675 || @@playerBullets[ch][1] < -75 || @@playerBullets[ch][1] > 675
+                  @@playerBullets.delete_at(ch)
+                  num += 1
+            end
+        end
+    end
+
     def self.movements(enemy_data)
         Level2.playerMovement
+        Level2.bulletMovements
         @@enemyX, @@enemyY = @@enemyData.move(@@enemyX, @@enemyY)
     end
 
@@ -64,9 +89,15 @@ class Level2
     def self.draw
         Level2.movements(@@enemyData)
         Window.draw(0, 0, @@backgroundImageLvl2)
+        @@playerImage = Image.load("image/playerYoru#{@@playerDirection}.png")
+        @@playerBulletImage = Image.load("image/playerBulletImage#{@@playerDirection}.png")
         Window.draw(@@playerX, @@playerY, @@playerImage)
         if !Level2.die?(@@enemyData, 1)
             Window.draw(@@enemyX, @@enemyY, @@enemyImage)
+        end
+
+        @@playerBullets.size.times do |i|
+            Window.draw(@@playerBullets[i][0], @@playerBullets[i][1], @@playerBulletImage)
         end
         
     end
