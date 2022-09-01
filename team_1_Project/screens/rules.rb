692afd26323146fafa1c83f5bb4d9d3b38cd2729
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-01 09:46:33 +0530

    Updated: Rules screen with background and text

--- team_1_Project/screens/rules.rb
+++ team_1_Project/screens/rules.rb
@@ -1,14 +1,30 @@
 class Rules
-    def initialize
-        @backgroundImage = Image.load("image/background.png")
-        @rectengle = Image.load("image/Rectangle 1.png")
-        @rule = Image.load("image/RUles.png")
-    end
+    @@backgroundImage = Image.load('image/background.png')
+    @@secondaryBackground = Image.load('image/secondaryBackground3.png')
+    @@rulesText = Image.load('image/rules_text.png')
+    @@loremText = Image.load('image/loremText.png')
 
+    # g
+
+    
+
+    # def update(x, y)
+    #     m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
+    #     if m === @@changeButton
+    #         @@changeButton = Sprite.new(277,334, Image.load('image/changeHover.png'))
+    #     end
+    # end
 
     def draw
-        Window.draw(0, 0, @backgroundImage)
-        Window.draw(20, 20, @rectengle)
-        Window.draw(260, 40, @backgroundImage)
+        # x = Input.mouse_pos_x
+        # y = Input.mouse_pos_y
+
+        # update(x, y)
+        Window.draw(0, 0, @@backgroundImage)
+        Window.draw(96, 84, @@secondaryBackground)
+        Window.draw(252,100,@@rulesText)
+        Window.draw(122,177,@@loremText)
+        # Window.draw(264,282,@@avatarText)
+        # Sprite.draw([@@changeButton])
     end
 end
