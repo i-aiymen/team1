7a892a98ed53fd26fa7cdd7fe458d9403bc5c158
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-01 09:50:22 +0530

    Updated: rules screen with back button

--- team_1_Project/screens/rules.rb
+++ team_1_Project/screens/rules.rb
@@ -4,27 +4,28 @@ class Rules
     @@rulesText = Image.load('image/rules_text.png')
     @@loremText = Image.load('image/loremText.png')
 
-    # g
+    def initialize
+        @@backButton = Sprite.new(13,17, Image.load('image/back.png'))
+    end
 
     
 
-    # def update(x, y)
-    #     m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
-    #     if m === @@changeButton
-    #         @@changeButton = Sprite.new(277,334, Image.load('image/changeHover.png'))
-    #     end
-    # end
+    def update(x, y)
+        m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
+        if m === @@backButton
+            @@backButton = Sprite.new(13,17, Image.load('image/backHover.png'))
+        end
+    end
 
     def draw
-        # x = Input.mouse_pos_x
-        # y = Input.mouse_pos_y
+        x = Input.mouse_pos_x
+        y = Input.mouse_pos_y
 
-        # update(x, y)
+        update(x, y)
         Window.draw(0, 0, @@backgroundImage)
         Window.draw(96, 84, @@secondaryBackground)
         Window.draw(252,100,@@rulesText)
         Window.draw(122,177,@@loremText)
-        # Window.draw(264,282,@@avatarText)
-        # Sprite.draw([@@changeButton])
+        Sprite.draw([@@backButton])
     end
 end
