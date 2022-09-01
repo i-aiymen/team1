a28563e80a02cb1dccc5e1aaf97a948543ddd4f1
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-02 01:08:47 +0530

    Updated: player and enemy image in lvl2 screen

--- team_1_Project/level/level2.rb
+++ team_1_Project/level/level2.rb
@@ -1,7 +1,11 @@
 class Level2
     @@backgroundImageLvl2 = Image.load('image/backgroundLvl2.png')
+    @@playerImage = Image.load('image/playerYoru.png')
+    @@enemyImage = Image.load('image/enemyRobot.png')
 
     def draw
         Window.draw(0, 0, @@backgroundImageLvl2)
+        Window.draw(60, 531, @@playerImage)
+        Window.draw(590, 51, @@enemyImage)
     end
 end
