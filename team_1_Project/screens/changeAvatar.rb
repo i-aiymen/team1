9e36bb17aa0e97b0aba1597a118ffc8605089c62
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-01 06:53:10 +0530

    Updated: Hover effect to avatarName buttons in changeAvatarScreen

--- team_1_Project/screens/changeAvatar.rb
+++ team_1_Project/screens/changeAvatar.rb
@@ -11,18 +11,24 @@ class ChangeAvatar
 
     
 
-    # def update(x, y)
-    #     m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
-    #     if m === @@changeButton
-    #         @@changeButton = Sprite.new(277,334, Image.load('image/changeHover.png'))
-    #     end
-    # end
+    def update(x, y)
+        m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
+        if m === @@avatarNameButton1
+            @@avatarNameButton1 = Sprite.new(118,444, Image.load('image/avatarName1Hover.png'))
+        end
+        if m === @@avatarNameButton2
+            @@avatarNameButton2 = Sprite.new(291,444, Image.load('image/avatarName2Hover.png'))
+        end
+        if m === @@avatarNameButton3
+            @@avatarNameButton3 = Sprite.new(464,444, Image.load('image/avatarName3Hover.png'))
+        end
+    end
 
     def draw
-        # x = Input.mouse_pos_x
-        # y = Input.mouse_pos_y
+        x = Input.mouse_pos_x
+        y = Input.mouse_pos_y
 
-        # update(x, y)
+        update(x, y)
         Window.draw(0, 0, @@backgroundImage)
         Window.draw(36, 42, @@secondaryBackground)
         Window.draw(199,65,@@settingsText)
