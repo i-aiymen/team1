b42b373a578b366e8e2a162e7802661b40318646
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-09-02 07:23:22 +0530

    Navigation: Routed from gameOver.rb to main and nickname screen

--- team_1_Project/screens/gameOver.rb
+++ team_1_Project/screens/gameOver.rb
@@ -13,9 +13,15 @@ class GameOver
         m = Sprite.new(x, y, Image.new(1, 1, C_BLACK))
         if m === @@homeButton
             @@homeButton = Sprite.new(290,380, Image.load('image/homeHover.png'))
+            if Input.mouse_push?(M_LBUTTON)
+                $flag = 0
+            end
         end
         if m === @@replayButton
             @@replayButton = Sprite.new(370,380, Image.load('image/replayHover.png'))
+            if Input.mouse_push?(M_LBUTTON)
+                $flag = 1
+            end
         end
     end
 
